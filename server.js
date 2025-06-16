8a8e0784beaee40b59d67bc17a4c221b18a3f516
Brodyachi <maks.zabrodin.04@gmail.com>
2025-06-16 17:02:00 +0400

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://127.0.0.1:5173',
-    'https://your-frontend-app.onrender.com'
+    'https://my-tts-app-frontend-vite.onrender.com/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
